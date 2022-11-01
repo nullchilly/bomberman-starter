aef726106a35d4e384ad6e9548242de8ffff2830
Null Chilly <nullchilly@gmail.com>
2022-11-01 10:30:26 +0700

    chore: Update readme

--- src/entities/character/Player.java
+++ src/entities/character/Player.java
@@ -2,7 +2,7 @@ package entities.character;
 
 import javafx.scene.image.Image;
 
-public abstract class Player extends Entity{
+public abstract class Player extends Entity {
     public Player(int x, int y, Image Img) {
         super(x, y, Img);
     }
